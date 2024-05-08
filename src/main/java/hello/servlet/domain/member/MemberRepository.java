925dcb3902c4a5aef2abcf49e3b36da9e3a850b9
김유진 <gracecancho@gmail.com>
2024-05-08 10:55:34 +0900

    서ë블릿으로 회원 관리 웹 애플리케이션 만들기

---
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,42 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 회원 저장소
+ */
+public class MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>(); //static 사용
+    private static long sequence = 0L; //static 사용
+
+    // singleton
+    private static final MemberRepository instance = new MemberRepository();
+
+    private MemberRepository() {
+    }
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
