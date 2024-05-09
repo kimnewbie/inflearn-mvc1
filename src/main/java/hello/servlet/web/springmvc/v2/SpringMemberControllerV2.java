bbe37f2d85f57b181c202d23f10f90dde36d627a
김유진 <gracecancho@gmail.com>
2024-05-09 10:18:18 +0900

    스프링 mvc - ì실용적인 방식

--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -1,2 +1,51 @@
-package hello.servlet.web.springmvc.v2;public class SpringMemberControllerV2 {
+package hello.servlet.web.springmvc.v2;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * 조합 결과
+ *      클래스 레벨 @RequestMapping("/springmvc/v2/members")
+ *          메서드 레벨 @RequestMapping("/new-form") /springmvc/v2/members/new-form
+ *          메서드 레벨 @RequestMapping("/save") /springmvc/v2/members/save
+ *          메서드 레벨 @RequestMapping /springmvc/v2/members
+ *
+ * @RequestMapping 클래스 레벨과 메서드 레벨 조합
+ */
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form") // /springmvc/v2/members/new-form
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+        ModelAndView mav = new ModelAndView("save-result");
+        mav.addObject("member", member);
+        return mav;
+    }
+
+    // /springmvc/v2/members 라서 RequestMapping에 추가 안해줘도 된다
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mav = new ModelAndView("members");
+        mav.addObject("members", members);
+        return mav;
+    }
 }
